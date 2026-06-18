58faf2138ae10079c80710157794a27546cbb6dc
Ayushi828 <aayushi.aa613@gmail.com>
2026-06-18 22:56:29 +0530

    log returns calculated

---
+++ src/returns.py
@@ -0,0 +1,9 @@
+from prices import (adj_close_price)
+import numpy as np
+
+
+def cal_logs(adj_close_price):
+  log_returns = np.log(adj_close_price/adj_close_price.shift(1))
+  log_returns = log_returns.dropna()
+
+  return log_returns
