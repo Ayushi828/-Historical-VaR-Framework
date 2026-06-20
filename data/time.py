d924b50f64c9702fa90299cec524573e7a53655f
Ayushi828 <aayushi.aa613@gmail.com>
2026-06-20 21:03:47 +0530

    Delete

--- data/time.py
+++
@@ -1,6 +0,0 @@
-import datetime as dt
-
-def time_range(years):
-  end_date = dt.datetime.now()
-  start_date = end_date - dt.timedelta(days = 365 * years)
-  print(f"The time range for data is {start_date.date()} to {end_date.date()}")
