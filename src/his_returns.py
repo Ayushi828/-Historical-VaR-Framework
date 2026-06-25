204adbe9afff415f3ab9b4078c0ab20e337c29e7
Ayushi828 <aayushi.aa613@gmail.com>
2026-06-25 23:29:19 +0530

    Cleanup function
    Removed print statements for historical returns.

--- src/his_returns.py
+++ src/his_returns.py
@@ -1,10 +1,6 @@
-from returns import (logs)
 
 
 def get_hist_returns(log_returns, wt):
   historical_returns = (log_returns * wt).sum(axis=1)
   
   return historical_returns
-
-  print("\n","-"*10, "\t HISTORICAL RETURNS\t", "-"*10)
-  print(historical_returns)
