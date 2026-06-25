84c2565a50ca5909532e14f9f7a64a7123caef3d
Ayushi828 <aayushi.aa613@gmail.com>
2026-06-25 23:30:31 +0530

    update var_historical

--- src/var_historical.py
+++ src/var_historical.py
@@ -1,10 +1,8 @@
 
-
-
 import numpy as np
-from x_returns import (xday_return_calc)
 
-def cal_var_s(xdays_return):
-  VAR_s = -np.percentile(xdays_return, 100 - (confidence*100)) * portf_value
+
+def cal_var_s(xdays_return, c, p_value):
+  VAR_s = -np.percentile(xdays_return, 100 - (c*100)) * p_value
 
   return VAR_s
