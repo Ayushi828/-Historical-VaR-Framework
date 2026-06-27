9576789c5e5aebd168b3d6305383ab62718f6b95
Ayushi828 <aayushi.aa613@gmail.com>
2026-06-27 20:26:31 +0530

    create setuppy

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name = "git_var_his", version = " 0.1", packages = find_packages())
+
+
+
+
+
+# "pip install e" in terminal window to setup the path
